82a962e4b169aee1458b2c722fde1a93ec526f1b
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2024-01-18 00:45:30 +0700

    [FEAT] Add history and production fee

--- src/app/(navbar)/history/components/seed-purchased.tsx
+++ src/app/(navbar)/history/components/seed-purchased.tsx
@@ -1,31 +1,33 @@
+import { OrderItem } from "@/types/orderItem";
 import Image from "next/image";
-const SeedPurchased = () => {
+
+const SeedPurchased = ({ orderItem, orderStatus }: { orderItem: OrderItem[], orderStatus: string }) => {
   return (
     <div className="w-[49%] aspect-[2/1] rounded-lg bg-white overflow-hidden">
       <div className="w-full h-full flex items-center py-5 px-10 gap-12">
         <div className="flex flex-col items-center">
           <div className="w-28 aspect-square rounded-full relative overflow-hidden mb-3">
             <Image
-              src={"/benih/jagung.jpg"}
+              src={orderItem[0].product.foto[0]}
               fill
               className="w-full h-full object-cover"
               alt="farmer"
             />
           </div>
-          <h1 className="font-dm font-bold text-lg">Benih Jagung</h1>
+          <h1 className="font-dm font-bold text-lg">{orderItem[0].product.nama}</h1>
 
           <p className="font-semibold">
-            10 <span className="font-dm text-gray-400">x</span> 500kg
+            {orderItem[0].quantity} <span className="font-dm text-gray-400">x</span> {orderItem[0].product.berat}
           </p>
         </div>
         <div className="flex-1">
           <div className="flex justify-between mt-auto w-full">
             <p className="font-semibold text-gray-400">Price</p>
-            <p className="font-semibold font-dm">Rp100.000</p>
+            <p className="font-semibold font-dm">Rp{orderItem[0].product.harga.toLocaleString()}</p>
           </div>
           <div className="flex justify-between w-full">
             <p className="font-semibold text-gray-400">Status</p>
-            <p className="font-semibold font-dm">In Progress</p>
+            <p className="font-semibold font-dm">{orderStatus}</p>
           </div>
           <div className="justify-between w-full mt-6 text-center">
             <p className="font-semibold text-gray-400">Planting Session</p>
